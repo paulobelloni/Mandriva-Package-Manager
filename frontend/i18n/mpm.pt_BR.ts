f2822679b30e73157ff362565489c40f949305e3
Paulo <paulo@mandriva.com>
2011-06-20 15:15:39 -0300

    Add Qt i18n (ts) files to: es,fr,it,pt_BR (the only completed) and ru.

---
+++ frontend/i18n/mpm.pt_BR.ts
@@ -0,0 +1,262 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_BR" sourcelanguage="en_US">
+<context>
+    <name>CategoryModel</name>
+    <message>
+        <location filename="../config/CategoryModel.qml" line="23"/>
+        <source>Accessories</source>
+        <translation>Acessórios</translation>
+    </message>
+    <message>
+        <location filename="../config/CategoryModel.qml" line="27"/>
+        <source>Development</source>
+        <translation>Desenvolvimento</translation>
+    </message>
+    <message>
+        <location filename="../config/CategoryModel.qml" line="31"/>
+        <source>Education</source>
+        <translation>Educação</translation>
+    </message>
+    <message>
+        <location filename="../config/CategoryModel.qml" line="35"/>
+        <source>Games</source>
+        <translation>Jogos</translation>
+    </message>
+    <message>
+        <location filename="../config/CategoryModel.qml" line="39"/>
+        <source>Internet</source>
+        <translation>Internet</translation>
+    </message>
+    <message>
+        <location filename="../config/CategoryModel.qml" line="43"/>
+        <source>Multimedia</source>
+        <translation>Multimídia</translation>
+    </message>
+    <message>
+        <location filename="../config/CategoryModel.qml" line="47"/>
+        <source>Office</source>
+        <translation>Escritório</translation>
+    </message>
+    <message>
+        <location filename="../config/CategoryModel.qml" line="51"/>
+        <source>Sciences</source>
+        <translation>Ciências</translation>
+    </message>
+    <message>
+        <location filename="../config/CategoryModel.qml" line="55"/>
+        <source>System</source>
+        <translation>Sistema</translation>
+    </message>
+</context>
+<context>
+    <name>FilterGroupsModel</name>
+    <message>
+        <location filename="../config/FilterGroupsModel.qml" line="22"/>
+        <source>Category</source>
+        <translation>Categoria</translation>
+    </message>
+    <message>
+        <location filename="../config/FilterGroupsModel.qml" line="26"/>
+        <source>Status</source>
+        <translation>Estado</translation>
+    </message>
+    <message>
+        <location filename="../config/FilterGroupsModel.qml" line="30"/>
+        <source>Source</source>
+        <translation>Fonte</translation>
+    </message>
+</context>
+<context>
+    <name>MainToolbar</name>
+    <message>
+        <location filename="../qml/private/MainToolbar.qml" line="39"/>
+        <source>Back</source>
+        <translation>Retornar</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/MainToolbar.qml" line="72"/>
+        <source>Next</source>
+        <translation>Avançar</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/MainToolbar.qml" line="119"/>
+        <source>History</source>
+        <translation>História</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/MainToolbar.qml" line="133"/>
+        <source>Settings</source>
+        <translation>Configurações</translation>
+    </message>
+</context>
+<context>
+    <name>PackageDetails</name>
+    <message>
+        <location filename="../qml/private/PackageDetails.qml" line="70"/>
+        <source>Category/Subgroup</source>
+        <translation>Categoria/Subgrupo</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageDetails.qml" line="83"/>
+        <source>Source</source>
+        <translation>Fonte</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageDetails.qml" line="96"/>
+        <source>Media</source>
+        <translation>Mídia</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageDetails.qml" line="111"/>
+        <source>Installed Time</source>
+        <translation>Data de Instalação</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageDetails.qml" line="124"/>
+        <source>Current Version</source>
+        <translation>Versão Atual</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageDetails.qml" line="137"/>
+        <source>Current Release (Epoch)</source>
+        <translation>Release Atual (Epoch)</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageDetails.qml" line="159"/>
+        <source>Architecture</source>
+        <translation>Arquitetura</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageDetails.qml" line="178"/>
+        <source>Size</source>
+        <translation>Tamanho</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageDetails.qml" line="269"/>
+        <source>Displaying package description is under construction ...</source>
+        <translation>A exibição da descrição do pacote ainda está em construção...</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageDetails.qml" line="284"/>
+        <source>More</source>
+        <translation>Mais</translation>
+    </message>
+    <message>
+        <source>More...</source>
+        <translation type="obsolete">Mais...</translation>
+    </message>
+</context>
+<context>
+    <name>PackageToolbar</name>
+    <message>
+        <location filename="../qml/private/PackageToolbar.qml" line="41"/>
+        <source>UPGRADE</source>
+        <translation>ATUALIZAR</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageToolbar.qml" line="62"/>
+        <source>INSTALL</source>
+        <translation>INSTALAR</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/PackageToolbar.qml" line="71"/>
+        <source>REMOVE</source>
+        <translation>REMOVER</translation>
+    </message>
+</context>
+<context>
+    <name>SearchBox</name>
+    <message>
+        <location filename="../qml/private/SearchBox.qml" line="35"/>
+        <source>type to search</source>
+        <translation>digite para pesquisar</translation>
+    </message>
+</context>
+<context>
+    <name>SourceModel</name>
+    <message>
+        <location filename="../config/SourceModel.qml" line="23"/>
+        <source>Mandriva</source>
+        <translation>Mandriva</translation>
+    </message>
+    <message>
+        <location filename="../config/SourceModel.qml" line="27"/>
+        <source>Community</source>
+        <translation>Comunidade</translation>
+    </message>
+</context>
+<context>
+    <name>StatusFilterButton</name>
+    <message>
+        <location filename="../qml/private/StatusFilterButton.qml" line="63"/>
+        <source>Show</source>
+        <translation>Mostra</translation>
+    </message>
+</context>
+<context>
+    <name>StatusModel</name>
+    <message>
+        <location filename="../config/StatusModel.qml" line="23"/>
+        <source>Installed</source>
+        <translation>Instalado</translation>
+    </message>
+    <message>
+        <location filename="../config/StatusModel.qml" line="27"/>
+        <source>Not-Installed</source>
+        <translation>Não-Instalado</translation>
+    </message>
+    <message>
+        <location filename="../config/StatusModel.qml" line="31"/>
+        <source>Upgrade</source>
+        <translation>Atualizável</translation>
+    </message>
+    <message>
+        <location filename="../config/StatusModel.qml" line="35"/>
+        <source>Transition</source>
+        <translation>Em transição</translation>
+    </message>
+</context>
+<context>
+    <name>TargetView</name>
+    <message>
+        <location filename="../qml/private/TargetView.qml" line="160"/>
+        <source>Name / Summary</source>
+        <translation>Nome / Sumário</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/TargetView.qml" line="174"/>
+        <source>Version</source>
+        <translation>Versão</translation>
+    </message>
+    <message>
+        <location filename="../qml/private/TargetView.qml" line="182"/>
+        <source>Size</source>
+        <translation>Tamanho</translation>
+    </message>
+</context>
+<context>
+    <name>TargetViewDelegateNameSummary</name>
+    <message>
+        <location filename="../qml/private/TargetViewDelegateNameSummary.qml" line="33"/>
+        <source>waiting...</source>
+        <translation>esperando...</translation>
+    </message>
+</context>
+<context>
+    <name>TextField</name>
+    <message>
+        <location filename="../qml/private/custom-qtdesktop/custom/TextField.qml" line="151"/>
+        <source>Enter text</source>
+        <translation>Entre com o texto</translation>
+    </message>
+</context>
+<context>
+    <name>TotalOfMatches</name>
+    <message>
+        <location filename="../qml/private/TotalOfMatches.qml" line="40"/>
+        <source>packages available</source>
+        <translation>pacotes disponíveis</translation>
+    </message>
+</context>
+</TS>
